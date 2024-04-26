02f277b40556c5fe3118ca991b0c3e934748ee89
zilionn <correiafernando165@gmail.com>
2024-04-26 10:45:27 -0300

    Context made.

---
+++ src/Fornecedor.Data/Context/SupplierContext.cs
@@ -0,0 +1,20 @@
+﻿using Fornecedor.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fornecedor.Data.Context {
+    internal class SupplierContext : DbContext {
+
+        public SupplierContext(DbContextOptions options) : base(options) { 
+        
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
